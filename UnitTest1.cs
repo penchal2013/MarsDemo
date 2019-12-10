a13c446c729ab89390ebde890400078c5768154e
penchal2013 <32608078+penchal2013@users.noreply.github.com>
2019-12-10 15:27:55 +0530

    created 1test
    tc created

---
+++ UnitTest1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+
+namespace SeleniumProject1
+{
+    [TestClass]
+    public class SeleTest1
+    {
+        private TestContext testContextInstance;
+        private IWebDriver driver;
+        private string appURL;
+
+        [TestMethod]
+        [TestCategory("Chrome")]
+        public void TheBingSearchTest()
+        {
+#pragma warning disable CA2234 // Pass system uri objects instead of strings
+            driver.Navigate().GoToUrl(appURL + "/");
+#pragma warning restore CA2234 // Pass system uri objects instead of strings
+
+            //driver.FindElement(By.Name("q")).SendKeys("Azure Pipelines");
+           Assert.IsTrue(driver.Title.Contains("Google"), "Verified title of the page");
+        }
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestInitialize()]
+        public void SetupTest()
+        {
+            appURL = "https://www.google.com/";
+
+            string browser = "Chrome";
+            switch (browser)
+            {
+                case "Chrome":
+                    driver = new ChromeDriver();
+                    break;
+                case "Firefox":
+                    driver = new FirefoxDriver();
+                    break;
+                case "IE":
+                    driver = new InternetExplorerDriver();
+                    break;
+                default:
+                    driver = new ChromeDriver();
+                    break;
+            }
+
+        }
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            driver.Quit();
+        }
+    }
+}
